515501129651ab1724dd8fd2da20e647dafb4b47
Ivan Klimenko <kli-van@yandex.ru>
2017-09-16 04:26:30 +0300

    add layout of intro authorization page

--- src/scripts/app.js
+++
@@ -1,3 +0,0 @@
-const dontGoPlease = require('./common/btn');
-
-dontGoPlease();
--- src/scripts/common/btn.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var element = document.querySelector('.button');
-  var prevent = function (e) {e.preventDefault()}
-  element.addEventListener('click', prevent);
-  })();
-
-module.exports = dontGoPlease;
