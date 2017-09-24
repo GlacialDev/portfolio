9a45ed882500d35b3a9dd023a0e5bc553683253c
Ivan Klimenko <kli-van@yandex.ru>
2017-09-24 12:04:59 +0300

    added hamMenu js-module

--- src/scripts/common/hamMenu.js
+++ src/scripts/common/hamMenu.js
@@ -1,12 +1,14 @@
 function hamMenu() {
-  const button = document.querySelector('.header__hamburger-menu'),
-        menu = document.querySelector('.hamburger-menu');
+  const menu = document.querySelector('.hamburger-menu'),
+        button = document.querySelector('.header__hamburger-menu'),
+        body = document.body;
 
   button.addEventListener('click', e => {
     e.preventDefault();
     button.classList.toggle('active');
     menu.classList.toggle('active');
-  })
-}
+    body.classList.toggle('overflow');
+  });
+};
 
 export default hamMenu;
---
+++ src/scripts/common/slider.js
@@ -0,0 +1,7 @@
+function slider() {
+  const prevSlide = document.querySelector('slider__bottom-left')
+        currentSlide = document.querySelector('slider__top'),
+        nextSlide = document.querySelector('slider__bottom-right')
+}
+
+export default slider;
