052912850bac7c34fdc222c45cce3c63b37e7c96
Ivan Klimenko <kli-van@yandex.ru>
2017-09-24 15:28:05 +0300

    added part of slider script (2)

--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -1,5 +1,7 @@
 function slider() {
-  const prevSlideScreen = document.querySelector('.slider__bottom-left'),
+  const slider = document.querySelector('.slider'),
+        control = document.querySelector('.slider__btn'),
+        prevSlideScreen = document.querySelector('.slider__bottom-left'),
         currentSlideScreen = document.querySelector('.slider__top'),
         nextSlideScreen = document.querySelector('.slider__bottom-right');
         //список картинок в слайдере
@@ -18,11 +20,11 @@ function slider() {
         nextSlideNumber = 2;
 
         // метод жесткого консоль-логгинга
-        // console.log('------------- start');
-        // console.log(prevSlideNumber);
-        // console.log(currentSlideNumber);
-        // console.log(nextSlideNumber);
-        // console.log('------------- ');
+        console.log('------------- start');
+        console.log(prevSlideNumber);
+        console.log(currentSlideNumber);
+        console.log(nextSlideNumber);
+        console.log('------------- ');
 
   // если число вылазит за максимальный номер в массиве, отправляем в начало (0)
   // иначе просто инкрементируем
@@ -53,13 +55,13 @@ function slider() {
     nextSlideNumber = changeToNextNumber(nextSlideNumber);
 
     // метод жесткого консоль-логгинга
-    // console.log(prevSlideNumber);
-    // console.log(currentSlideNumber);
-    // console.log(nextSlideNumber);
-    // console.log('-------------');
-    // console.log('url(../images/works/slides/'+imagesList[prevSlideNumber]+');');
-    // console.log('url(../images/works/slides/'+imagesList[currentSlideNumber]+');');
-    // console.log('url(../images/works/slides/'+imagesList[nextSlideNumber]+');');
+    console.log(prevSlideNumber);
+    console.log(currentSlideNumber);
+    console.log(nextSlideNumber);
+    console.log('-------------');
+    console.log('url(../images/works/slides/'+imagesList[prevSlideNumber]+');');
+    console.log('url(../images/works/slides/'+imagesList[currentSlideNumber]+');');
+    console.log('url(../images/works/slides/'+imagesList[nextSlideNumber]+');');
 
     //подставляем в бекграунд картинки, расположенные под новыми номерами, из массива
     prevSlideScreen.style.backgroundImage = 'url(../images/works/slides/'+imagesList[prevSlideNumber]+');';
@@ -67,10 +69,13 @@ function slider() {
     nextSlideScreen.style.backgroundImage = 'url(../images/works/slides/'+imagesList[nextSlideNumber]+');';
   }
 
-  //тестим работу
-  moveSlide();
-  moveSlide();
-  moveSlide();
+  // если на страничке есть слайдер, вешаем слушатель
+  if(slider) {
+    control.addEventListener('click', e => {
+      e.preventDefault();
+      moveSlide();
+    })
+  }
 }
 
 export default slider;
