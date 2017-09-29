a9b016128963bb1072553eacc9b5fb6eda016933
Ivan Klimenko <kli-van@yandex.ru>
2017-09-30 01:54:11 +0300

    added beta of preloader script (fix 2)

--- src/scripts/common/preloader.js
+++ src/scripts/common/preloader.js
@@ -1,11 +1,17 @@
 'use strict';
 
 function preloader() {
-  const preloader = document.querySelector('.preloader');
+  const preloader = document.querySelector('.preloader'),
+        body = document.body;
 
+  // отключаем скролл пока не загрузится страница
+  body.classList.toggle('overflow');
+
+  // когда контент загружается, убираем прелоадер и разрешаем скролл
   window.onload = function() {
     preloader.classList.add('contentLoaded');
     preloader.style.display="none";
+    body.classList.toggle('overflow');
   };
 
 }
